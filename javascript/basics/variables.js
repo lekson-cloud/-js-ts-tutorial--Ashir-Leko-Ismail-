9951f81cbbdbfcad0de04f0754a0dca3c57a6e01
Ashir Leko Ismail <lekson1992.cs@gmail.com>
2026-02-08 09:09:37 -0800

    Variables and Datatypes

---
+++ javascript/basics/variables.js
@@ -0,0 +1,16 @@
+let name = "Ashir Leko Ismail"
+let age = 33
+let city = "Kaduna"
+let isStudent = true
+
+let person = {
+    name: name,
+    age: age,
+    city: city,
+    isStudent: isStudent
+}
+
+let hoobies = ["Football", "Tenis", "Basket Ball"]
+
+console.log("This is the person's Info: ", person)
+console.log("This are my favorite sports", hoobies)
